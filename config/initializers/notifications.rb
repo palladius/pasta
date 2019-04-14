a95f3569a86aac15c09c7c559f5e836622aaaca9
Riccardo Carlesso <palladiusbonton@gmail.com>
2019-04-14 16:24:47 +0200

    muted notifications too

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,4 +1,6 @@
-# Set our own default Notifier
-module ActiveSupport::Notifications
-  self.notifier = Notifier.new(Fanout.new(true))
-end
+# 2019-04-14 Gives error if i dont comment out :/
+
+# # Set our own default Notifier
+# module ActiveSupport::Notifications
+#   self.notifier = Notifier.new(Fanout.new(true))
+# end
