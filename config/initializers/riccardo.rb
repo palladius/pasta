52f9cbbce5994dd3ad930ca40e2ef635c988b21b
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-11-20 17:19:42 +0100

    fixed footer

--- config/initializers/riccardo.rb
+++ config/initializers/riccardo.rb
@@ -5,3 +5,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
+
+APP_NAME = 'Pasta DoneWell'
+APP_VERSION = File.read("#{Rails.root}/VERSION").chomp
