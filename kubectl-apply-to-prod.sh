dd6138dee5ed585cb38511ec9e35f4be9844b13f
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-11-27 16:09:57 +0100

    nearly there...

--- kubectl-apply-to-prod.sh
+++ kubectl-apply-to-prod.sh
@@ -11,6 +11,8 @@ done
 # kubectl-get-gcp-url from sakura
 # DEV_SERVICE="service/pastang-manhouse-dev"
 # PROD_SERVICE="service/pastang-manhouse-prod"
+echo "💛 Quick health-check:"
+kubectl get deploy | grep pastang-manhouse
 
 GCLB_IP_DEV=$(kubectl get  service/pastang-manhouse-dev  --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 GCLB_IP_PROD=$(kubectl get service/pastang-manhouse-prod --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
@@ -18,6 +20,13 @@ GCLB_IP_PROD=$(kubectl get service/pastang-manhouse-prod --output jsonpath='{.st
 echo "👷‍♂️DEV LB IP: $GCLB_IP_DEV"
 echo "🌳PRD LB IP: $GCLB_IP_PROD"
 
+if kubectl get deploy | grep pastang-manhouse | grep '0/' ; then
+    echo '😥 Looks like some issues with your Deployments. Wont continue further'
+    exit 42
+fi
 
 echo "Note: this will only work for Riccardo. For inspiration, check kubernetes-service.rb in palladius/sakura"
+# gcloud --quiet --project XXX beta dns record-sets create --rrdatas='1.2.3.4' --type=A --ttl=300 --zone=palladi-us pastang-dev.palladi.us
 dns-setup-palladius.sh pastang-dev.palladi.us  "$GCLB_IP_DEV"
+dns-setup-palladius.sh pastang.palladi.us      "$GCLB_IP_PROD"
+
