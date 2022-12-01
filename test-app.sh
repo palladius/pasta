75884726ededc254d58c4b350c8f60c94ba21cf0
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-12-01 19:30:33 +0100

    just looks slightly better

--- test-app.sh
+++ test-app.sh
@@ -4,17 +4,19 @@ SERVICE_REGEX="skpasta-"
 
 
 function _curl() {
-    echodo curl -I --max-time 0.9 -s "$1" 2>/dev/null
+    SERVICE="$1"
+    URL="$2"
+    echo "[🏚️🐛] curling $SERVICE => $URL"
+    echodo curl -I --max-time 0.9 -s "$URL" # 2>/dev/null
 }
 
 #kubectl get 'pod,deployment' | grep skpasta
 kubectl get service | grep "$SERVICE_REGEX" | while read service type intip EXT_IP port_mapping time ; do
     PORT=$(echo $port_mapping | cut -d: -f 1)
-    echo "[🏚️🐛] check $service $EXT_IP:$PORT"
 
 
     echo
-    _curl "http://$EXT_IP:$PORT/"
+    _curl "$service" "http://$EXT_IP:$PORT/"
 #    _curl "http://$EXT_IP:8080"
 done
 
