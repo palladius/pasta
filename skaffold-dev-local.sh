2f1880306950769ff3b1457c523607568e96e1d3
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-01-21 12:06:51 +0100

    minor

--- skaffold-dev-local.sh
+++ skaffold-dev-local.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# 1. set minukube
 
+start_minikube() {
 # TODO: find if necessary
 	minikube start --profile custom
 	skaffold config set --global local-cluster true
 	eval $(minikube -p custom docker-env)
+}
+
+
+# 1. set minukube
+#start_minikube
 
 # 2. skaffold dev
 #    skaffold dev
-
-
 export STATE=$(git rev-list -1 HEAD --abbrev-commit)
 skaffold build --file-output build-$STATE.json
-
-echo "1. Now try: skaffold deploy -a build-$STATE.json"
+echo "2. Built: build-$STATE.json"
+echo "3. Now try: skaffold deploy -a build-$STATE.json"
 
 # how do u get the 2d83b59?
 # latest from `skaffold build`
